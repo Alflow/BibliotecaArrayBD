fd298d479d5850a84b1ba59b4d30a4cfc900fe29
Alfredo Alberruche Herrero <aalberrh@gmail.com>
2024-01-23 13:54:35 +0100

    Funcionalidad completa

--- fuente/Repositorio/GestionBdRepositorio.php
+++ fuente/Repositorio/GestionBdRepositorio.php
@@ -37,6 +37,8 @@ class GestionBdRepositorio
                 //     $library[$fila['codigo']['genero']] = $fila['genero'];
                 //     $library[$fila['codigo']['prestado']] = $fila['prestado'];
                 // }
+
+
             } catch (\PDOException $ex) {
                 throw $ex;
             } finally {
@@ -105,7 +107,98 @@ class GestionBdRepositorio
         }
         return [];
     }
-}
 
+
+    public function devolverLibrosPrestados(array $librosParaDevolver)
+    {
+        require_once __DIR__ . '/../../core/ConexionBd.inc';
+        
+        // preparo sentencia de modificación en LIBROS.
+        $sql_1 = 'UPDATE libro SET prestado = 0 WHERE codigo = :codigo'; 
+        
+        // preparo sentencia de modificación en LIBROS PRESTADOS (ELIMINACIÓN).
+        $sql_2 = 'DELETE FROM libroPrestado WHERE codigo = :codigo'; 
+
+        try {
+            $con = (new ConexionBd())->getConexion();
+            $con->beginTransaction(); // Iniciar una transacción
+            $snt = $con->prepare($sql_1); // Preparar la sentencia
+
+            foreach ($librosParaDevolver as $codigo) {
+                $snt->bindParam(':codigo', $codigo, PDO::PARAM_STR);
+                $snt->execute();
+            }
+
+            //Pido a snt que prepare la segunda Query
+            $snt = $con->prepare($sql_2);
+            foreach ($librosParaDevolver as $codigo) {
+                $snt->bindParam(':codigo', $codigo, PDO::PARAM_STR);
+                $snt->execute();
+            }
+
+            $con->commit(); // Confirmar la transacción
+        } catch (\PDOException $ex) {
+            if (isset($con)) {
+                $con->rollback(); // Revertir la transacción en caso de error
+            }
+            throw $ex;
+        } finally {
+            if (isset($snt)) {
+                $snt = null; // Cierra el statement
+            }
+            if (isset($con)) {
+                $con = null; // Cierra la conexión
+            }
+        }
+    }
+
+    public function pedirLibrosPrestados(array $librosParaPedir){
+        require_once __DIR__ . '/../../core/ConexionBd.inc';
+        
+        // preparo sentencia de modificación en LIBROS.
+        $sql_1 = 'UPDATE libro SET prestado = 1 WHERE codigo = :codigo'; 
+        
+        // preparo sentencia de modificación en LIBROS PRESTADOS (ELIMINACIÓN).
+        $sql_2 = 'INSERT INTO libroPrestado (codigo, eCorreo) VALUES (:codigo ,:eCorreo)'; 
+
+        try {
+            $con = (new ConexionBd())->getConexion();
+            $con->beginTransaction(); // Iniciar una transacción
+            $snt = $con->prepare($sql_1); // Preparar la sentencia
+
+            foreach ($librosParaPedir as $codigo) {
+                $snt->bindParam(':codigo', $codigo, PDO::PARAM_STR);
+                $snt->execute();
+            }
+
+            //Pido a snt que prepare la segunda Query
+            $snt = $con->prepare($sql_2);
+            foreach ($librosParaPedir as $codigo) {
+                $snt->bindParam(':codigo', $codigo, PDO::PARAM_STR );
+                $correo = $_SESSION['usuario'];
+                $snt->bindParam(':eCorreo',$correo, PDO::PARAM_STR,);
+                $snt->execute();
+            }
+
+            $con->commit(); // Confirmar la transacción
+
+            
+        } catch (\PDOException $ex) {
+            if (isset($con)) {
+                $con->rollback(); // Revertir la transacción en caso de error
+            }
+            throw $ex;
+        } finally {
+            if (isset($snt)) {
+                $snt = null; // Cierra el statement
+            }
+            if (isset($con)) {
+                $con = null; // Cierra la conexión
+            }
+        }
+    }
+
+
+}
 
 // Tenemos que conseguir devolver leyendo de la base de datos.
